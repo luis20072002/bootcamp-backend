7ceecb60088da0a0a21f073ccdc5181925b8fc07
dei0811 <deinergonzalezparedes12@gmail.com>
2026-03-14 15:10:05 -0500

    4 cruds añadidos

---
+++ API/main.py
@@ -0,0 +1,253 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import hashlib
+
+
+# NOTA: SEPARAR MODELOS A OTRO ARCHIVO DIFERENTE AL MAIN
+# SOLO DEBERIAN HABER ENDPOINTS EN ESTE ARCHIVO
+
+app = FastAPI()
+
+# --- MODELOS ---
+
+class Aula(BaseModel):
+    nombre: str
+    edificio: str
+    capacidad: int
+
+
+class Usuario(BaseModel):
+    nombre: str
+    pwsd: str
+    admin: bool = False
+
+
+class UsuarioUpdate(BaseModel):
+    nuevo_nombre: str
+
+class Docente(BaseModel):
+    id : int
+    nombre : str
+    apellido : str
+    correo : str
+    telefono: str
+
+class Curso(BaseModel):
+    id : int
+    nombre : str
+    codigo : str
+    id_docente : int
+
+# --- BASES DE DATOS SIMULADAS ---
+
+aula: list[dict] = [] #check
+usuarios: list[dict] = [] #check
+
+cursos: list[dict] = [] # check
+Horarios_trabajo: list[dict] = []
+turnos: list[dict] = []
+Planilla_trabajo: list[dict] = []
+docentes: list[dict] = [] #check
+Registros_aula: list[dict] = []
+
+
+# --- AULA ---
+
+@app.post("/aula")
+def subiraula(datos: Aula):
+
+    nueva_aula = {
+        "id": len(aula),
+        "nombre": datos.nombre,
+        "edificio": datos.edificio,
+        "capacidad": datos.capacidad
+    }
+
+    aula.append(nueva_aula)
+
+    return {"mensaje": "aula creada", "aula": nueva_aula}
+
+
+@app.get("/aula/{id}")
+def get_aula_by_id(id: int):
+
+    for registro in aula:
+        if registro["id"] == id:
+            return registro
+
+    return {"error": "aula no encontrada"}
+
+
+@app.put("/aula/{id}")
+def actualizaraula(id: int, datos: Aula):
+
+    for registro in aula:
+        if registro["id"] == id:
+            registro["nombre"] = datos.nombre
+            registro["edificio"] = datos.edificio
+            registro["capacidad"] = datos.capacidad
+
+            return {"mensaje": "aula actualizada", "aula": registro}
+
+    return {"error": "aula no encontrada"}
+
+
+@app.delete("/aula/{id}")
+def eliminaraula(id: int):
+
+    for i, registro in enumerate(aula):
+        if registro["id"] == id:
+            aula.pop(i)
+            return {"mensaje": "aula eliminada"}
+
+    return {"error": "aula no encontrada"}
+
+
+# --- USUARIOS ---
+
+@app.post("/usuario")
+def crear_usuario(datos: Usuario):
+
+    hash_pwsd = hashlib.sha256(datos.pwsd.encode()).hexdigest()
+
+    usuario = {
+        "id": len(usuarios),
+        "nombre": datos.nombre,
+        "password": hash_pwsd,
+        "rol": "administrador" if datos.admin else "auxiliar"
+    }
+
+    usuarios.append(usuario)
+
+    return {"mensaje": "usuario creado", "usuario": usuario}
+
+
+@app.get("/usuario/{id}")
+def get_usuario_by_id(id: int):
+
+    for usuario in usuarios:
+        if usuario["id"] == id:
+            return usuario
+
+    return {"error": "usuario no encontrado"}
+
+
+@app.put("/usuarios/{nombre}")
+def actualizar_nombre(nombre: str, datos: UsuarioUpdate):
+
+    for usuario in usuarios:
+        if usuario["nombre"] == nombre:
+            usuario["nombre"] = datos.nuevo_nombre
+            return {"mensaje": "usuario actualizado", "usuario": usuario}
+
+    return {"error": "usuario no encontrado"}
+
+
+@app.delete("/usuario/{id}")
+def delete_user_by_ID(id: int):
+      for i, registro in enumerate(usuarios):
+        if registro["id"] == id:
+            usuarios.pop(i)
+            return {"mensaje": "Usuario eliminado"}
+
+        return {"error": "Usuario no encontrado"}
+      
+# --- Docentes
+
+@app.post("/Docente")
+def add_docente(datos: Docente):
+    nuevo_docente={
+        "id" : Docente.id,
+        "nombre" : Docente.nombre,
+        "apellido" : Docente.apellido,
+        "correo" : Docente.correo,
+        "telefono" : Docente.telefono
+        }
+    docentes.append(nuevo_docente)
+
+@app.get("/Docente/{id}")
+def get_docente_by_id(id: int):
+     for docente in docentes:
+        if docente["id"] == id:
+            return docente
+
+        return {"error": "usuario no encontrado"}
+
+
+@app.put("/docente/{id}")
+def update_email(id: int, email: str):
+
+    for docente in docentes:
+        if docente["id"] == id:
+            docente["email"] = email
+            return {"mensaje": "email actualizado", "docente": docente}
+
+    return {"error": "docente no encontrado"}
+
+
+
+@app.delete("/Docente/{id}")
+def delete_docente_by_ID(id: int):
+      for i, registro in enumerate(docentes):
+        if registro["id"] == id:
+            usuarios.pop(i)
+            return {"mensaje": "Docente eliminado"}
+
+        return {"error": "Docente no encontrado"}
+      
+
+#--- Cursos ---
+
+@app.post("/Cursos/")
+def crear_curso(datos: Curso):
+
+    docente_existe = False
+
+    for docente in docentes:
+        if docente["id"] == datos.id_docente:
+            docente_existe = True
+            break
+
+    if not docente_existe:
+        return {"error": "Docente no encontrado"}
+    
+    curso = {
+        "id": len(cursos),
+        "nombre": datos.nombre,
+        "docente_id": datos.id_docente
+
+    }
+
+    cursos.append(curso)
+
+    return {"Mensaje": "Curso registrado con exito"}
+
+@app.get("/Cursos/{id}")
+def get_Curso_by_id(id: int):
+
+    for curso in cursos:
+        if curso["id"] == id:
+            return curso
+
+    return {"error": "Curso no encontrado"}
+
+@app.put("/Cursos/{id}")
+def actualizar_docente(id:int, docente_id: int):
+
+    for curso in cursos:
+        if curso["id"] == id:
+            curso["docente_id"] = docente_id
+            return {"mensaje": "docente actualizado", "curso": curso}
+
+    return {"error": "curso no encontrado"}
+
+
+@app.delete("/curso/{id}")
+def eliminar_curso(id: int):
+
+    for i, curso in enumerate(cursos):
+        if curso["id"] == id:
+            cursos.pop(i)
+            return {"mensaje": "curso eliminado"}
+
+    return {"error": "curso no encontrado"}
