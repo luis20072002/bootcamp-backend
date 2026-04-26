ef57cc1e7c2ef72ec7572764b2b775eeaa2538cc
dei0811 <deinergonzalezparedes12@gmail.com>
2026-04-25 21:56:54 -0500

    added env

--- api/database/database.py
+++ api/database/database.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 import pyodbc
-DATABASE_URL = "mssql+pyodbc://talento:cartagena@nodossolutions.com:1435/SGS?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
+from dotenv import load_dotenv
+import os
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+DATABASE_URL = "postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
 engine = create_engine(DATABASE_URL,pool_pre_ping=True)
 
